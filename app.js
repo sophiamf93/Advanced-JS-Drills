8d68db6ee47fa552623c9564f0b85530059380e2
sophiamf93 <sophia_mf@icloud.com>
2021-06-08 20:34:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// name = "Sophia";
+// var name;
+// console.log(name)
+
+// setName();
+// function setName() {
+//     var name = 'Sophia';
+//     console.log(name);
+// }
+
+// console.log('Sophia');
+// let avg = findAvg(2, 2);
+// console.log('Sam', avg);
+// function findAvg(a, b) {
+//     console.log('Mary');
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['Watermelon', 'Strawberries', 'Blueberries']
+// // let favFruit;
+
+
+
+// function printFruits() {
+//     let favFruit = fruits[2];
+//     console.log(fruits[0])
+
+//     function printFavFruits() {
+//         let leastFav = fruits[2]
+//         console.log(favFruit);
+//     }
+// }
+
+// console.log(leastFav)
+// printFruits();
+// // printFavFruit();
+
+// someFunc();
+// function someFunc() {
+//     var someFunc = 
+//     console.long('Hello, ', )
+// }
+
