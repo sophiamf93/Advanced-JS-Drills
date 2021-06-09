1142dbbfa9f03d15827ec3824eb4fbebe2a5b820
sophiamf93 <sophia_mf@icloud.com>
2021-06-09 11:27:17 -0500

    last commit

--- app.js
+++ app.js
@@ -4,41 +4,55 @@
 
 // setName();
 // function setName() {
-//     var name = 'Sophia';
-//     console.log(name);
-// }
-
-// console.log('Sophia');
-// let avg = findAvg(2, 2);
-// console.log('Sam', avg);
-// function findAvg(a, b) {
-//     console.log('Mary');
-//     var answer = (a + b) / 2;
-//     return answer;
-// }
-
-// let fruits = ['Watermelon', 'Strawberries', 'Blueberries']
-// // let favFruit;
-
-
-
-// function printFruits() {
-//     let favFruit = fruits[2];
-//     console.log(fruits[0])
-
-//     function printFavFruits() {
-//         let leastFav = fruits[2]
-//         console.log(favFruit);
-//     }
-// }
-
-// console.log(leastFav)
-// printFruits();
-// // printFavFruit();
-
-// someFunc();
-// function someFunc() {
-//     var someFunc = 
-//     console.long('Hello, ', )
-// }
-
+    //     var name = 'Sophia';
+    //     console.log(name);
+    // }
+    
+    // console.log('Sophia');
+    // let avg = findAvg(2, 2);
+    // console.log('Sam', avg);
+    // function findAvg(a, b) {
+        //     console.log('Mary');
+        //     var answer = (a + b) / 2;
+        //     return answer;
+        // }
+        
+        // let fruits = ['Watermelon', 'Strawberries', 'Blueberries']
+        // // let favFruit;
+        
+        
+        
+        // function printFruits() {
+            //     let favFruit = fruits[2];
+            //     console.log(fruits[0])
+            
+            //     function printFavFruits() {
+                //         let leastFav = fruits[2]
+                //         console.log(favFruit);
+                //     }
+                // }
+                
+                // console.log(leastFav)
+                // printFruits();
+                // // printFavFruit();
+                
+                // someFunc();
+                // function someFunc() {
+                    //     var someFunc = 
+                    //     console.long('Hello, ', )
+                    // }
+                    
+                    // name = "Sophia";
+                    // var name;
+                    
+                    // newFunc()
+                    
+                    // functions newFunc() {
+                    //     console.log('hello , ${name}')
+                    // }
+                    
+                    // let otherFunc = functions() {
+                    //     alert("hi")
+                    // }
+                    
+                    // otherFunc()
